392d26341460376a8218abec65727db25909a95b
parinitha25 <pnarayanaostb1@gmail.com>
2019-07-18 17:21:53 +0530

    forth compition

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -14,35 +14,49 @@ class Contact extends Component {
         </div>
         <div className="row">
           <div className="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-             <p className="taxfind"><i>How To Find Us?</i></p>  
-          <p className="taxhead"><i>Head Quarters</i></p> 
-          <p className="addrp">level 1/9 Parsons St, <br></br>Alice Springs NT 0870, Australia</p>   
+            <p className="taxfind"><i>How To Find Us?</i></p>  
+            <p className="taxhead"><i>Head Quarters</i></p> 
+            <p className="addrp">level 1/9 Parsons St, <br></br>Alice Springs NT 0870, Australia</p>   
+          </div>
+        </div>
         <div className="row">
-          <div className="col-xs-1 col-sm-1 col-md-1 col-lg-1"></div>
-          <p className="taxs"><i>Support Center</i></p> 
-          <div className="col-xs-5 col-sm-5 col-md-5 col-lg-5">    
-            <p className="custp"><b>Tax Helplines​</b></p>
-            <p className="custp">Aaykar Sampark Kendra (ASK)
+          <div className="col-xs-1 col-sm-1 col-md-1 col-lg-1"></div>  
+          <div className="col-xs-5 col-sm-5 col-md-5 col-lg-5">  
+          <p className="suptp"><i>Support Center</i></p>   
+            <p className="custpa"><b>Tax Helplines​</b></p>
+          <div className="row">
+          {/* <div className="col-xs-1 col-sm-1 col-md-1 col-lg-1"></div>   */}
+          <div className="col-xs-6 col-sm-6 col-md-6 col-lg-6"> 
+            <p className="custp">Aaykar Sampark Kendra 
             <img className="custimg" src={require('../images/customer.png')} alt="First slide" /></p>
-            <p>​​Income tax/PAN/TAN
+            <p className="cparagraph">​​Income tax/PAN/TAN
               General Queries</p><br/>
             <p className="custp">Jeanne Bliss
             <img className="custimg" src={require('../images/customerw.png')} alt="First slide" /></p>
-            <p>​​Income tax/
+            <p className="cparagrapha">​​Income tax/
             General Queries</p>
           </div>
-          <div className="col-xs-4 col-sm-4 col-md-4 col-lg-4">   
+          <div className="col-xs-3 col-sm-3 col-md-3 col-lg-3"> 
+            <div><img className="phoneimg" src={require('../images/phone.jpg')} alt="First slide" /></div> 
+            <div><img className="phoneimga" src={require('../images/phone.jpg')} alt="First slide" /></div>
+            </div>
+            <div className="col-xs-2 col-sm-2 col-md-2 col-lg-2"> 
+            <p className="number">18001801961/1961</p>
+          <p className="numbera">1800180546/6743</p>
+            </div>
+          </div>
+          </div>
+          {/* <div className="col-xs-4 col-sm-4 col-md-4 col-lg-4">   
             <div><img className="phoneimg" src={require('../images/phone.jpg')} alt="First slide" /></div> 
             <div><img className="phoneimga" src={require('../images/phone.jpg')} alt="First slide" /></div>
           </div> 
           <div className="col-xs-2 col-sm-2 col-md-2 col-lg-2"> 
               <p className="number">18001801961/1961</p>
           <p className="numbera">1800180546/6743</p>
-          </div>
-          <div className="col-xs-1 col-sm-1 col-md-1 col-lg-1"></div>
+          </div> */}
+          {/* <div className="col-xs-1 col-sm-1 col-md-1 col-lg-1"></div> */}
         </div>
-        </div>
-        </div>
+       
         <div className="row">
           <div className="col-xs-12 col-sm-12 col-md-12 col-lg-12">
           {/* <p className="taxhead"><i>Head Quarters</i></p>  */}
